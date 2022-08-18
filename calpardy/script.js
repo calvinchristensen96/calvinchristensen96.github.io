8f7bdb022a40a87aa3f7ab4d84ae167d430ec734
Calvin Christensen <calvinchristensen96@gmail.com>
2022-08-18 16:53:25 -0500

    Add files via upload

--- calpardy/script.js
+++ calpardy/script.js
@@ -1,27 +1,54 @@
 var foo;
-var category;
-var catid = Math.floor(Math.random() * 2132);
+var category = [];
 var tempIndex = 0;
+var tempCat = 0;
+var i;
 
-fetch('https://jservice.io/api/category?id=' + catid)
+for (i=0;i<6;i++) {
+	id = Math.floor(Math.random() * 2132);
+	getCategories(i, id);
+}
+
+setTimeout(function() {
+	updateCategories();
+}, 500);
+
+function updateCategories() {
+	for (i=0;i<6;i++) {
+		document.getElementById("cat" + i).innerHTML = category[i].title;
+	}
+}
+
+function getCategories(i, id) {
+fetch('https://jservice.io/api/category?id=' + id)
   .then(response => response.text())
   .then((data) => {
-  //  alert(data);
-    category = JSON.parse(data);
-    document.getElementById("cat0").innerHTML = category.title;
+    category.push(JSON.parse(data));
+    document.getElementById("cat" + i).innerHTML = category[i].title;
     }
   )
+}
 
-function showClue(index) {
+function showClue(cat, index) {
   document.getElementById("clue-modal").style.display = "inline-block";
-  document.getElementById("question").innerHTML = category.clues[index].question;
+  document.getElementById("question").innerHTML = category[cat].clues[index].question;
   tempIndex = index;
+  tempCat = cat;
 }
 
 function showAnswer() {
-  document.getElementById("question").innerHTML = category.clues[tempIndex].answer;
+  document.getElementById("question").innerHTML = category[tempCat].clues[tempIndex].answer;
 }
 
 function hideClue() {
   document.getElementById("clue-modal").style.display = "none";
+  document.getElementById(tempCat + "-" + (tempIndex+1) * 200).innerHTML = "";
+}
+
+function addScore(player) {
+	document.getElementById("score" + player).innerHTML = "$" + (tempIndex + 1) * 200;
+}
+
+function subScore(player) {
+
 }
