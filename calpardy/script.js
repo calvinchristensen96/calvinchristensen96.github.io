24a2861d98897a29d181d7aacfc14009af95e137
Calvin Christensen <calvinchristensen96@gmail.com>
2022-12-21 15:39:29 -0600

    Rehaul design

--- calpardy/script.js
+++ calpardy/script.js
@@ -1,3 +1,31 @@
+function start() {
+  	transition("title","game");
+	updateCategories();
+    	for (i=0;i<6;i++) {
+		document.getElementById(i + "-200").innerHTML = "$200";
+		document.getElementById(i + "-400").innerHTML = "$400";
+		document.getElementById(i + "-600").innerHTML = "$600";
+		document.getElementById(i + "-800").innerHTML = "$800";
+		document.getElementById(i + "-1000").innerHTML = "$1000";
+	}
+}
+
+function transition(to, from) {
+	var a = setTimeout(function() {
+		document.getElementById(to).style.display = "none";
+    		document.getElementById(from).style.display = "inline";
+    		document.getElementById("transition-layer").style.opacity = 0;
+    	}, 250);
+	document.getElementById("transition-layer").style.opacity = 1;
+}
+
+function closeQuestion() {
+	document.getElementById("question").style.display = "none";
+   	document.getElementById("game").style.display = "inline";
+   	document.getElementById(tempCat + "-" + (tempIndex+1) * 200).innerHTML = "";
+    	document.getElementById(tempCat + "-" + (tempIndex+1) * 200).style.backgroundColor = "white";
+}
+
 var foo;
 var category = [];
 var tempIndex = 0;
@@ -15,18 +43,17 @@ setTimeout(function() {
 
 function updateCategories() {
 	for (i=0;i<6;i++) {
-		document.getElementById("cat" + i).innerHTML = category[i].title.toUpperCase();
+		document.getElementById("cat-" + i).innerHTML = category[i].title.toUpperCase();
 	}
 }
 
 function getCategories(i, id) {
 fetch('https://jservice.io/api/category?id=' + id)
-  .then(response => response.text())
-  .then((data) => {
-    category.push(JSON.parse(data));
-    document.getElementById("cat" + i).innerHTML = category[i].title;
-    }
-  )
+	.then(response => response.text())
+  	.then((data) => {
+    		category.push(JSON.parse(data));
+    		document.getElementById("cat-" + i).innerHTML = category[i].title.toUpperCase();
+	})
 }
 
 function refreshCategories() {
@@ -40,7 +67,6 @@ function refreshCategories() {
 	setTimeout(function() {
 		updateCategories();
 	}, 500);
-
 	for (i=0;i<6;i++) {
 		document.getElementById(i + "-200").innerHTML = "$200";
 		document.getElementById(i + "-400").innerHTML = "$400";
@@ -51,25 +77,14 @@ function refreshCategories() {
 }
 
 function showClue(cat, index) {
-  document.getElementById("clue-modal").style.display = "inline-block";
-  document.getElementById("question").innerHTML = category[cat].clues[index].question + "<br><br>";
-  tempIndex = index;
-  tempCat = cat;
+  	document.getElementById("question").style.display = "inline";
+  	document.getElementById("game").style.display = "none";
+  	document.getElementById("question-category").innerHTML = category[cat].title.toUpperCase();
+  	document.getElementById("question-card").innerHTML = category[cat].clues[index].question;
+  	tempIndex = index;
+  	tempCat = cat;
 }
 
 function showAnswer() {
-  document.getElementById("question").innerHTML = category[tempCat].clues[tempIndex].answer;
-}
-
-function hideClue() {
-  document.getElementById("clue-modal").style.display = "none";
-  document.getElementById(tempCat + "-" + (tempIndex+1) * 200).innerHTML = "";
-}
-
-function addScore(player) {
-	document.getElementById("score" + player).innerHTML = "$" + (tempIndex + 1) * 200;
-}
-
-function subScore(player) {
-
+  	document.getElementById("question-card").innerHTML = category[tempCat].clues[tempIndex].answer;
 }
