15c25e19b2b8ef1056e5ab2c26b5625781072d5e
Calvin Christensen <calvinchristensen96@gmail.com>
2022-12-22 10:20:36 -0600

    Add airdate of clues
    Add the year of the clues' original airdates
    Grey out completed categories

--- calpardy/script.js
+++ calpardy/script.js
@@ -1,13 +1,9 @@
+var gameBoard = [[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]];
+
 function start() {
   	transition("title","game");
 	updateCategories();
-    	for (i=0;i<6;i++) {
-		document.getElementById(i + "-200").innerHTML = "$200";
-		document.getElementById(i + "-400").innerHTML = "$400";
-		document.getElementById(i + "-600").innerHTML = "$600";
-		document.getElementById(i + "-800").innerHTML = "$800";
-		document.getElementById(i + "-1000").innerHTML = "$1000";
-	}
+
 }
 
 function transition(to, from) {
@@ -23,7 +19,13 @@ function closeQuestion() {
 	document.getElementById("question").style.display = "none";
    	document.getElementById("game").style.display = "inline";
    	document.getElementById(tempCat + "-" + (tempIndex+1) * 200).innerHTML = "";
-    	document.getElementById(tempCat + "-" + (tempIndex+1) * 200).style.backgroundColor = "white";
+	document.getElementById(tempCat + "-" + (tempIndex+1) * 200).style.background = "none";
+    gameBoard[tempCat][tempIndex] = 0;
+	// Grey out a finished category
+    if (gameBoard[tempCat].every((val, i, arr) => val === 0 )) {
+    	document.getElementById("game-section-" + tempCat).style.color = "#454545";
+		document.getElementById("game-section-" + tempCat).style.backgroundColor = "grey";
+	}
 }
 
 var foo;
@@ -33,17 +35,34 @@ var tempCat = 0;
 var i;
 
 for (i=0;i<6;i++) {
-	id = Math.floor(Math.random() * 2132);
+	id = Math.floor(Math.random() * 5505);
 	getCategories(i, id);
 }
-
-setTimeout(function() {
-	updateCategories();
-}, 500);
 
 function updateCategories() {
 	for (i=0;i<6;i++) {
 		document.getElementById("cat-" + i).innerHTML = category[i].title.toUpperCase();
+        document.getElementById("cat-" + i + "-date").innerHTML = category[i].clues[0].airdate.substring(0,4);
+        if (category[i].clues.length > 0) {
+			document.getElementById(i + "-200").innerHTML = "$200";
+			document.getElementById(i + "-200").style.backgroundColor = "var(--c2)";
+        }
+        if (category[i].clues.length > 1) {
+			document.getElementById(i + "-400").innerHTML = "$400";
+			document.getElementById(i + "-400").style.backgroundColor = "var(--c2)";
+        }
+        if (category[i].clues.length > 2) {
+			document.getElementById(i + "-600").innerHTML = "$600";
+			document.getElementById(i + "-600").style.backgroundColor = "var(--c2)";
+        } 
+		if (category[i].clues.length > 3) {
+			document.getElementById(i + "-800").innerHTML = "$800";
+			document.getElementById(i + "-800").style.backgroundColor = "var(--c2)";
+		}
+		if (category[i].clues.length > 4) {
+			document.getElementById(i + "-1000").innerHTML = "$1000";
+			document.getElementById(i + "-1000").style.backgroundColor = "var(--c2)";
+		}
 	}
 }
 
@@ -51,8 +70,8 @@ function getCategories(i, id) {
 fetch('https://jservice.io/api/category?id=' + id)
 	.then(response => response.text())
   	.then((data) => {
-    		category.push(JSON.parse(data));
-    		document.getElementById("cat-" + i).innerHTML = category[i].title.toUpperCase();
+    	category.push(JSON.parse(data));
+    	document.getElementById("cat-" + i).innerHTML = category[i].title.toUpperCase();
 	})
 }
 
@@ -67,13 +86,6 @@ function refreshCategories() {
 	setTimeout(function() {
 		updateCategories();
 	}, 500);
-	for (i=0;i<6;i++) {
-		document.getElementById(i + "-200").innerHTML = "$200";
-		document.getElementById(i + "-400").innerHTML = "$400";
-		document.getElementById(i + "-600").innerHTML = "$600";
-		document.getElementById(i + "-800").innerHTML = "$800";
-		document.getElementById(i + "-1000").innerHTML = "$1000";
-	}
 }
 
 function showClue(cat, index) {
