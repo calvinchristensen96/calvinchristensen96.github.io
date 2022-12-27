560c9201b56706548cbe0a161becd85c331da070
Calvin Christensen <calvinchristensen96@gmail.com>
2022-12-27 11:50:07 -0600

    Test mobile layout

--- calpardy/script.js
+++ calpardy/script.js
@@ -1,8 +1,20 @@
 var gameBoard = [[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]];
 
-function start() {
-  transition("title","game");
-  updateCategories();
+function start(val) {
+	if (val == 0) {
+  	  transition("title","options");
+      document.getElementById("title-logo").style.top = "24px";
+      document.getElementById("title-logo").style.fontSize = "24px";
+      document.getElementById("header").style.display = "inline";
+	  updateCategories();
+    } else {
+      transition("title","game");
+      document.getElementById("title-logo").style.top = "24px";
+      document.getElementById("title-logo").style.fontSize = "24px";
+      document.getElementById("header").style.display = "inline";
+	  updateCategories();
+    }
+
 }
 
 var dark = false;
@@ -17,25 +29,25 @@ function changeTheme() {
 }
 
 function transition(to, from) {
-  var a = setTimeout(function() {
-    document.getElementById(to).style.display = "none";
-    document.getElementById(from).style.display = "inline";
-    document.getElementById("transition-layer").style.opacity = 0;
-  }, 250);
-  document.getElementById("transition-layer").style.opacity = 1;
+	var a = setTimeout(function() {
+		document.getElementById(to).style.display = "none";
+    		document.getElementById(from).style.display = "inline";
+    		document.getElementById("transition-layer").style.opacity = 0;
+    	}, 250);
+	document.getElementById("transition-layer").style.opacity = 1;
 }
 
 function closeQuestion() {
-  document.getElementById("question").style.display = "none";
-  document.getElementById("game").style.display = "inline";
-  document.getElementById(tempCat + "-" + (tempIndex+1) * 200).innerHTML = "";
-  document.getElementById(tempCat + "-" + (tempIndex+1) * 200).style.background = "none";
-  gameBoard[tempCat][tempIndex] = 0;
-  // grey out a finished category
-  if (gameBoard[tempCat].every((val, i, arr) => val === 0 )) {
-    document.getElementById("game-section-" + tempCat).style.color = "#454545";
-      document.getElementById("game-section-" + tempCat).style.opacity = "0.5";
-    }
+	document.getElementById("question").style.display = "none";
+   	document.getElementById("game").style.display = "inline";
+   	document.getElementById(tempCat + "-" + (tempIndex+1) * 200).innerHTML = "";
+	document.getElementById(tempCat + "-" + (tempIndex+1) * 200).style.background = "none";
+    gameBoard[tempCat][tempIndex] = 0;
+	// grey out a finished category
+    if (gameBoard[tempCat].every((val, i, arr) => val === 0 )) {
+    	document.getElementById("game-section-" + tempCat).style.color = "#454545";
+		document.getElementById("game-section-" + tempCat).style.opacity = "0.5";
+	}
 }
 
 var foo;
@@ -45,68 +57,68 @@ var tempCat = 0;
 var i;
 
 for (i=0;i<6;i++) {
-  id = Math.floor(Math.random() * 5505);
-  getCategories(i, id);
+	id = Math.floor(Math.random() * 5505);
+	getCategories(i, id);
 }
 
 function updateCategories() {
-  for (i=0;i<6;i++) {
-    document.getElementById("cat-" + i).innerHTML = category[i].title.toUpperCase();
-    document.getElementById("cat-" + i + "-date").innerHTML = category[i].clues[0].airdate.substring(0,4);
-    if (category[i].clues.length > 0) {
-      document.getElementById(i + "-200").innerHTML = "$200";
-      document.getElementById(i + "-200").style.backgroundColor = "var(--c2)";
-    }
-    if (category[i].clues.length > 1) {
-      document.getElementById(i + "-400").innerHTML = "$400";
-      document.getElementById(i + "-400").style.backgroundColor = "var(--c2)";
-    }
-    if (category[i].clues.length > 2) {
-      document.getElementById(i + "-600").innerHTML = "$600";
-      document.getElementById(i + "-600").style.backgroundColor = "var(--c2)";
-    }
-    if (category[i].clues.length > 3) {
-      document.getElementById(i + "-800").innerHTML = "$800";
-      document.getElementById(i + "-800").style.backgroundColor = "var(--c2)";
-    }
-    if (category[i].clues.length > 4) {
-      document.getElementById(i + "-1000").innerHTML = "$1000";
-      document.getElementById(i + "-1000").style.backgroundColor = "var(--c2)";
-    }
-  }
+	for (i=0;i<6;i++) {
+		document.getElementById("cat-" + i).innerHTML = category[i].title.toUpperCase();
+        document.getElementById("cat-" + i + "-date").innerHTML = category[i].clues[0].airdate.substring(0,4);
+        if (category[i].clues.length > 0) {
+			document.getElementById(i + "-200").innerHTML = "$200";
+			document.getElementById(i + "-200").style.backgroundColor = "var(--c2)";
+        }
+        if (category[i].clues.length > 1) {
+			document.getElementById(i + "-400").innerHTML = "$400";
+			document.getElementById(i + "-400").style.backgroundColor = "var(--c2)";
+        }
+        if (category[i].clues.length > 2) {
+			document.getElementById(i + "-600").innerHTML = "$600";
+			document.getElementById(i + "-600").style.backgroundColor = "var(--c2)";
+        }
+		if (category[i].clues.length > 3) {
+			document.getElementById(i + "-800").innerHTML = "$800";
+			document.getElementById(i + "-800").style.backgroundColor = "var(--c2)";
+		}
+		if (category[i].clues.length > 4) {
+			document.getElementById(i + "-1000").innerHTML = "$1000";
+			document.getElementById(i + "-1000").style.backgroundColor = "var(--c2)";
+		}
+	}
 }
 
 function getCategories(i, id) {
 fetch('https://jservice.io/api/category?id=' + id)
-  .then(response => response.text())
-  .then((data) => {
-  category.push(JSON.parse(data));
-  document.getElementById("cat-" + i).innerHTML = category[i].title.toUpperCase();
-  })
+	.then(response => response.text())
+  	.then((data) => {
+    	category.push(JSON.parse(data));
+    	document.getElementById("cat-" + i).innerHTML = category[i].title.toUpperCase();
+	})
 }
 
 function refreshCategories() {
-  category = [];
-  tempIndex = 0;
-  tempCat = 0;
-  for (i=0;i<6;i++) {
-    id = Math.floor(Math.random() * 2132);
-    getCategories(i, id);
-  }
-  setTimeout(function() {
-    updateCategories();
-  }, 500);
+	category = [];
+	tempIndex = 0;
+	tempCat = 0;
+	for (i=0;i<6;i++) {
+		id = Math.floor(Math.random() * 2132);
+		getCategories(i, id);
+	}
+	setTimeout(function() {
+		updateCategories();
+	}, 500);
 }
 
 function showClue(cat, index) {
-  document.getElementById("question").style.display = "inline";
-  document.getElementById("game").style.display = "none";
-  document.getElementById("question-category").innerHTML = category[cat].title.toUpperCase();
-  document.getElementById("question-card").innerHTML = category[cat].clues[index].question;
-  tempIndex = index;
-  tempCat = cat;
+  	document.getElementById("question").style.display = "inline";
+  	document.getElementById("game").style.display = "none";
+  	document.getElementById("question-category").innerHTML = category[cat].title.toUpperCase();
+  	document.getElementById("question-card").innerHTML = category[cat].clues[index].question;
+  	tempIndex = index;
+  	tempCat = cat;
 }
 
 function showAnswer() {
-  document.getElementById("question-card").innerHTML = category[tempCat].clues[tempIndex].answer;
+  	document.getElementById("question-card").innerHTML = category[tempCat].clues[tempIndex].answer;
 }
