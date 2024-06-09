890c0673678f94ba91b9ce6dc6c25fbf9925e5de
Calvin Christensen <calvinchristensen96@gmail.com>
2024-06-08 23:55:00 -0500

    Update script.js

--- Rainbow Code Jam Glitch0624/script.js
+++ Rainbow Code Jam Glitch0624/script.js
@@ -1,4 +1,5 @@
 var colorIds = ["p-red", "p-orange", "p-yellow", "p-green", "p-blue", "p-purple"];
+var colorArray = ["red", "orange", "yellow", "green", "blue", "purple"];
 var colorPercents = [0, 0, 0, 0, 0, 0];
 
 var i = 0;
@@ -33,3 +34,29 @@ var f = setTimeout(function() {
   document.getElementById(colorIds[i]).style.width = ((Math.floor(Math.random() * 7) + 4) * 10) + "%" ;
   i++;
 }, 520);
+
+function begin() {
+  document.getElementById("scene-intro").style.display = "none";
+  document.getElementById("scene-goals").style.display = "inline";
+  document.getElementById("title").innerHTML = "Rainbow Fitness";
+  document.getElementById("subtitle").style.display = "none";
+}
+
+var goal = 0;
+function advanceGoals() {
+  if (goal < 6) {
+    document.getElementById("ct-" + goal).style.borderBottom = ("solid 0px " + colorArray[goal]);
+    document.getElementById("goals" + goal).style.display = "none";
+    document.getElementById("goal-" + goal).value = document.getElementById("goals-your-" + goal).value;
+    goal++;
+    document.getElementById("goals" + goal).style.display = "inline";
+    document.getElementById("ct-" + goal).style.borderBottom = ("solid 2px " + colorArray[goal]);
+  } else {
+    submitGoals();
+  }
+}
+
+function submitGoals() {
+  document.getElementById("scene-goals").style.display = "none";
+  document.getElementById("scene-rainbow").style.display = "inline";
+}
